470a75d90a5517455c046fb22c5bc564f4d6e6d7
Max Lapan <max.lapan@gmail.com>
2014-10-06 09:29:23 +0400

    Started resolver
    This module should perform the following tasks:
    1. do multithreading resolution of large amount of domain list
    2. perform deduplication of IP-Name pairs
    3. filter invalid names (127.0.0.1, nonexistent, etc)

---
+++ resolver/resolver.py
@@ -0,0 +1,16 @@
+import sys
+import logging
+import argparse
+import socket
+import Queue as queue
+
+
+if __name__ == "__main__":
+	logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s: %(message)s")
+
+	parser = argparse.ArgumentParser(description="Perform parallel resolution of domain names to IP addresses")
+	parser.add_argument("domains_files", type=str, nargs="+", help="File name with domains to be processed")
+	parser.add_argument("--reversed", action="store_true", help="Process domains in reversed-dot notation. I.e: ru.yandex.www")
+
+	args = parser.parse_args()
+	print args
