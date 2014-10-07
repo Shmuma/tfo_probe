3b50ba84c84422043b6a854dd04d608ddd481125
Max Lapan <max.lapan@gmail.com>
2014-10-07 11:41:22 +0400

    Finished with data

--- resolver/resolver.py
+++ resolver/resolver.py
@@ -10,8 +10,16 @@ if __name__ == "__main__":
 	logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s: %(message)s")
 
 	parser = argparse.ArgumentParser(description="Perform parallel resolution of domain names to IP addresses")
-	parser.add_argument("domains_files", type=str, nargs="+", help="File name with domains to be processed")
-	parser.add_argument("--reversed", action="store_true", help="Process domains in reversed-dot notation. I.e: ru.yandex.www")
+	parser.add_argument("domains", type=str, nargs=1, help="File name with domains to be processed")
+	parser.add_argument("--reversed", action="store_true",
+		help="Process domains in reversed-dot notation. I.e: ru.yandex.www")
+	parser.add_argument("--threads", action="store", type=int, help="Count of threads to run", default=8)
+	parser.add_argument("--no-stats", action="store_true", default=False, help="Suppres stats after run")
 
 	args = parser.parse_args()
 	print args
+	
+	with open(args.domains[0], 'r') as fd:
+		domains_stream = data.DomainsFilterStream(fd, reversed=args.reversed)
+		for domain_txt in domains_stream:
+			print domain_txt
